46d685c6158fd2af088d7c2177ad492af96bc5ef
Andy <hoyle020@umn.edu>
2023-12-05 20:06:10 -0600

    figured out idea for p2

--- scripts/pointer.gd
+++ scripts/pointer.gd
@@ -1,7 +1,11 @@
 extends RigidBody3D
 
 @onready var left_controller: XRController3D = $XROrigin3D/LeftController
+@onready var point_two: MeshInstance3D = $XROrigin3D/LeftController/PointTwo
 var is_active: bool = false
+var point_incrementation = 0.1
+var midpoint: Vector3 = Vector3.ZERO
+var distance: int = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,21 +19,30 @@ func quadratic_bezier(p0: Vector3, p1: Vector3, p2: Vector3, t: float):
 	
 func generate_points(start, mid, end):
 	var points = Array() 
-	for t in range(0, 1, 0.1):
+	for t in range(0.1, 1 + point_incrementation, point_incrementation):
 		points.append(quadratic_bezier(start, mid, end, t))
 	return points
+
+func alter_meshes(points):
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if is_active:
-		var points = []
-		var start = left_controller.position
-		# need to find p2 (or end)		
+		var start = left_controller.global_position
+		var end = point_two.global_position
+		var mid = (start + end) / 2.0
+		var points = generate_points(start, mid, end)
+		print(start, mid, end)
+		print(points)
 	
 func _button_pressed(name: String):
 	if name == "trigger_click":
 		is_active = true
+		midpoint
+		# Build/move meshes here for the given points on the first iteration
 	
 func _button_released(name: String):
 	if name == "trigger_click":
 		is_active = false
+		# set meshes to invisible
